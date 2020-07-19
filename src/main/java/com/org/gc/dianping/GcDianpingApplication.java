64bda81dc8ede46c4a07ab76accf865f7f9517d9
574876372@qq.com <574876372@qq.com>
2020-07-19 08:42:07 +0800

    ok

---
+++ src/main/java/com/org/gc/dianping/GcDianpingApplication.java
@@ -0,0 +1,13 @@
+package com.org.gc.dianping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcDianpingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GcDianpingApplication.class, args);
+    }
+
+}
